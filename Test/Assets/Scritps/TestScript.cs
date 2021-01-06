fc44f0ba366796c62d99fc32029636956f707012
kmiecikJob <76965636+kmiecikJob@users.noreply.github.com>
2021-01-06 14:02:42 +0100

    Test from visual stuio

--- Test/Assets/Scritps/TestScript.cs
+++ Test/Assets/Scritps/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Test from visual studio");
     }
 
     // Update is called once per frame
